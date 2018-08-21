50536d389278d5de323192e679614052410771e9
kookyungmin <rudals4549@naver.com>
2018-08-21 11:50:40 +0900

    update

--- src/main/java/com/gguri/swp/service/BoardService.java
+++ src/main/java/com/gguri/swp/service/BoardService.java
@@ -5,11 +5,11 @@ import java.util.List;
 import com.gguri.swp.domain.BoardVO;
 
 public interface BoardService {
-	public void regist(BoardVO board) throws Exception;
-	public BoardVO read(Integer bno) throws Exception;
-	public void modify(BoardVO board) throws Exception;
-	public void remove(Integer bno) throws Exception;
-	public List<BoardVO> listAll() throws Exception;
+	void regist(BoardVO board) throws Exception;
+	BoardVO read(Integer bno) throws Exception;
+	void modify(BoardVO board) throws Exception;
+	void remove(Integer bno) throws Exception;
+	List<BoardVO> listAll() throws Exception;
 	//dummy 추가
-	public void dummy() throws Exception;
+	void dummy() throws Exception;
 }
