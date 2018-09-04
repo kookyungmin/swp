26e2a7735febe7f184a1ec8fb4f8dfed9a0a0c38
kookyungmin <rudals4549@naver.com>
2018-09-04 18:15:27 +0900

    update

--- src/main/webapp/WEB-INF/views/test.jsp
+++ src/main/webapp/WEB-INF/views/test.jsp
@@ -23,17 +23,48 @@
 			내용 : <textarea name="replytext" id="newReplyText" cols="30" rows="3"></textarea>
 		</div>
 		<button id="btnReplyAdd" class="btn btn-primary">등록</button>
+		<button id="btnListAll" class="btn btn-warning">댓글전체JSON</button>
+	</div>
+	
+	<div id="modDiv">
+		<div class="modal-title">
+			<div>
+				<textarea id="replycontext" rows="3" placeholder="Enter"></textarea>	
+			</div>
+		</div>
+		<div>
+			<button id="btnModReply">수정</button>
+			<button id="btnDelReply">삭제</button>
+			<button id="btnCloseReply">닫기</button>
+		</div>
 	</div>
 	
 <script src="/resources/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<script src="/resources/test.js"></script>
+<script src="/resources/test.js?ver=3"></script>
 <script>
 	$(function(){
 		//$('#h2-title').on('click',listAll);
 		listAll();
 		$('#btnReplyAdd').click(function(){
 			registerReply();
-		})	
+		})
+		$('#btnListAll').click(function(){
+			let result = [];
+			$('#replies li').each ( (idx, li) => {
+				let $li = $(li),
+					rno = $li.data('rno')
+					replyer = $li.data('replyer')
+					replytext = $li.text().replace(/[\n\r\t]/g,'').trim(); //정규식 /g를 안 붙이면 \n 만나는 첫번째 것만 바꿈
+				result.push({
+					rno: rno,
+					replyer: replyer,
+					replytext: replytext
+			    })
+			})
+			result = JSON.stringify(result, null, '  ');
+			console.log(result);
+		})
+		movCenterModDiv();
 	});
 </script>
 </body>
--- src/main/webapp/resources/test.js
+++ src/main/webapp/resources/test.js
@@ -1,60 +1,66 @@
 function registerReply(){
 	const BNO = 6;
 	const REGIST_URL = "/replies";
-	let $replyer = $('#newReplyWriter'),
-		$replytext = $('#newReplyText'),
-		$errorFocus = null;
 	
-	let replyer = $replyer.val(),
+	let jsonData = getValidData($('#newReplyWriter'),$('#newReplyText'));
+	jsonData.bno = BNO;
+	
+	sendAjax(REGIST_URL, (isSuccess, responseText) => {
+		if(isSuccess){
+			alert("등록이 완료 되었습니다.");
+			listAll();
+		}else{
+			if(!isSuccess){
+				alert("오류가 발생하였습니다. (errorMessage:" + res + ")");
+			}
+		}
+	} , jsonData, 'POST');
+}
+
+function getValidData($replyer, $replytext){
+	let errorFocus = null,
+		replyer = $replyer.val(),
 		replytext = $replytext.val(),
 		errorMsg = "";
-	
+
 	if(!replyer){
 		errorMsg = "작성자를 입력하세요.";
 		$errorFocus = $replyer;
-	}else if(!replytext){
+	} else if(!replytext){
 		errorMsg = "내용을 입력하세요";
 		$errorFocus = $replytext;
 	}
+	
 	if(errorMsg){
 		alert(errorMsg);
 		$errorFocus.focus();
 		return;
 	}
 	
-	let jsonData = {
-			bno      : BNO,
-			replyer  : replyer,
-			replytext: replytext
-	};
-	let sender = $.ajax({
-		method: 'POST',
-		url: REGIST_URL,
-		contentType: "application/json",
-		data: JSON.stringify(jsonData),
-	})
+	return {replyer: replyer, replytext: replytext};
+}
+
+function sendAjax(url, fn, jsonData, method){
+	let options = {
+						method: method,
+						url: url,
+						contentType: "application/json"
+				   };
+	//jsonData가 있을 때만 data : JSON.stringify(jsonData) 추가
+	if(jsonData){
+		options.data = JSON.stringify(jsonData);
+		console.log(options);
+	}
 	
-	sender.always((responseText,statusText,ajaxResult) => {
-		if(statusText === 'success'){
-			alert("등록 되었습니다.");
-			listAll();
-		}else{
-			alert("오류가 발생하였습니다!");
+	$.ajax(options).always((responseText, statusText, res) =>{
+		let isSuccess = statusText === 'success'; //ajax 호출 성공 여부
+		fn(isSuccess, responseText);
+		if(!isSuccess){
+			alert("오류가 발생하였습니다. (errorMessage:" + res + ")");
 		}
 	})
 }
 
-/*function listAll(){
-var bno = 6,
-listUrl = "/replies/all/" + bno;
-$.getJSON(listUrl, function(data, b, c){ 
-	console.log(">> data=", data, ", b=", b, ", c=", c);
-	c.always(function(){
-		console.log(c.status);
-	})
-});	
-}
-*/
 
 function listAll(){
 	const bno = 6; 
@@ -62,9 +68,8 @@ function listAll(){
 	listUrl = "/replies/all/" + bno + "/" + page;
 	$.getJSON(listUrl, (data, b, c) => { 
 		console.log(">> data=", data, ", b=", b, ", c=", c);
-		//$(data).each((a,b) => {console.log(a,b)});
-		
 		let str = ""; //바뀔 수 있음
+		//$(data).each((a,b) => {console.log(a,b)});
 		data.list.forEach(
 			(d) => {
 				//str += '<li data-rno="' + d.rno + '" class="replyLi">' + d.replytext + '<button>수정</button>' + '</li>';
@@ -76,6 +81,12 @@ function listAll(){
 		);
 		$('#replies').html(str);
 	});
+}
+
+function movCenterModDiv(){
+	$modDiv = $('#modDiv');
+	$modDiv.css({'margin-left':$modDiv.width()/2*(-1)});
+	$modDiv.css({'margin-top':$modDiv.height()/2*(-1)});
 }
 
 function modClicked(btn){
